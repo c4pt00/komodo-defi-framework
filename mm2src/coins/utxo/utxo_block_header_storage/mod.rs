ea778d2a6f70f85f2281b382b09333e63017c5da
Samuel Onoja <samiodev@icloud.com>
2024-04-19 22:06:15 +0100

    WIP utxo_block_header storage refacting

--- mm2src/coins/utxo/utxo_block_header_storage/mod.rs
+++ mm2src/coins/utxo/utxo_block_header_storage/mod.rs
@@ -60,14 +60,19 @@ impl BlockHeaderStorage {
     pub(crate) fn new_from_ctx(
         ctx: MmArc,
         ticker: String,
-        _db_id: Option<&str>,
+        db_id: Option<&str>,
     ) -> Result<Self, BlockHeaderStorageError> {
+        use core::panicking::panic;
         use db_common::sqlite::rusqlite::Connection;
         use std::sync::{Arc, Mutex};
 
+        let db_id = db_id.map(|e| e.to_string()).unwrap_or_else(|| ctx.rmd160_hex());
         let conn = Arc::new(Mutex::new(Connection::open_in_memory().unwrap()));
-        let conn = ctx.sqlite_connection.clone_or(conn);
+        let mut connections = HashMap::new();
+        connecions.insert(db_id, conn);
+        let conn = ctx.sqlite_connection_v2.clone_or(Arc::new(Mutex::new(connections)));
 
+        let conn = ctx.sqlite_connection_res_v2(db_id).unwrap();
         Ok(BlockHeaderStorage {
             inner: Box::new(SqliteBlockHeadersStorage { ticker, conn }),
         })
