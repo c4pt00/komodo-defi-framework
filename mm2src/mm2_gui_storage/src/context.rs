35b102a961515b4d5dd4e6316459a94b4a46a474
Samuel Onoja <samiodev@icloud.com>
2024-05-06 18:26:29 +0100

    add todos for AccountContext

--- mm2src/mm2_gui_storage/src/context.rs
+++ mm2src/mm2_gui_storage/src/context.rs
@@ -2,6 +2,7 @@ use crate::account::storage::{AccountStorage, AccountStorageBoxed, AccountStorag
 use mm2_core::mm_ctx::{from_ctx, MmArc};
 use std::sync::Arc;
 
+#[allow(unused)]
 pub(crate) struct AccountContext {
     storage: AccountStorageBoxed,
     db_id: Option<String>,
@@ -9,30 +10,14 @@ pub(crate) struct AccountContext {
 
 impl AccountContext {
     /// Obtains a reference to this crate context, creating it if necessary.
+    /// TODO: this is only create/intiliaze once..need to find a way to manage multiple account contexts
     pub(crate) fn from_ctx(ctx: &MmArc, db_id: Option<&str>) -> Result<Arc<AccountContext>, String> {
-        let account_context = from_ctx(&ctx.account_ctx, move || {
+        from_ctx(&ctx.account_ctx, move || {
             Ok(AccountContext {
-                storage: AccountStorageBuilder::new(ctx, db_id)
-                    .build()
-                    .map_err(|e| e.to_string())?,
+                storage: AccountStorageBuilder::new(ctx, db_id).build().map_err(|e| e.to_string())?,
                 db_id: db_id.map(|e| e.to_string()),
             })
-        })?;
-
-        if account_context.db_id.as_deref() != db_id {
-            let mut ctx_field = ctx.account_ctx.lock().unwrap();
-            let account_context = Arc::new(AccountContext {
-                storage: AccountStorageBuilder::new(ctx, db_id)
-                    .build()
-                    .map_err(|e| e.to_string())?,
-                db_id: db_id.map(|e| e.to_string()),
-            });
-            *ctx_field = Some(Arc::clone(&account_context) as Arc<dyn std::any::Any + Send + Sync>);
-
-            return Ok(account_context);
-        };
-
-        Ok(account_context)
+        })
     }
 
     /// Initializes the storage and returns a reference to it.
