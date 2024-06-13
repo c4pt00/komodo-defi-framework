73833f03e57fa34042f2e1f55120da4922d0486c
Alright <mzlot555@gmail.com>
2024-06-13 03:24:26 -0400

    add additional encoding tests

--- mm2src/coins/sia/transaction.rs
+++ mm2src/coins/sia/transaction.rs
@@ -1,10 +1,11 @@
 use crate::sia::address::Address;
 use crate::sia::encoding::{Encodable, Encoder};
 use crate::sia::spend_policy::{SpendPolicy, UnlockCondition};
-use ed25519_dalek::Signature;
+use ed25519_dalek::{PublicKey, Signature};
 use rpc::v1::types::H256;
 
-#[cfg(test)] use ed25519_dalek::PublicKey;
+#[cfg(test)]
+use crate::sia::spend_policy::{spend_policy_atomic_swap_refund, spend_policy_atomic_swap_success, PolicyTypeThreshold};
 #[cfg(test)] use std::str::FromStr;
 
 type SiacoinOutputID = H256;
@@ -332,35 +333,254 @@ fn test_siacoin_input_encode_v1() {
 }
 
 #[test]
-#[ignore] // FIXME WIP
+fn test_signature_encode() {
+    let signature = Signature::from_bytes(
+        &hex::decode("105641BF4AE119CB15617FC9658BEE5D448E2CC27C9BC3369F4BA5D0E1C3D01EBCB21B669A7B7A17CF8457189EAA657C41D4A2E6F9E0F25D0996D3A17170F309").unwrap()).unwrap();
+
+    let hash = Encoder::encode_and_hash(&signature);
+    let expected = H256::from("1e6952fe04eb626ae759a0090af2e701ba35ee6ad15233a2e947cb0f7ae9f7c7");
+    assert_eq!(hash, expected);
+}
+
+#[test]
+fn test_satisfied_policy_encode_public_key() {
+    let public_key = PublicKey::from_bytes(
+        &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
+    )
+    .unwrap();
+
+    let policy = SpendPolicy::PublicKey(public_key);
+
+    let signature = Signature::from_bytes(
+        &hex::decode("105641BF4AE119CB15617FC9658BEE5D448E2CC27C9BC3369F4BA5D0E1C3D01EBCB21B669A7B7A17CF8457189EAA657C41D4A2E6F9E0F25D0996D3A17170F309").unwrap()).unwrap();
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec![signature],
+        preimages: vec![],
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("51832be911c7382502a2011cbddf1a9f689c4ca08c6a83ae3d021fb0dc781822");
+    assert_eq!(hash, expected);
+}
+
+#[test]
+fn test_satisfied_policy_encode_hash_empty() {
+    let policy = SpendPolicy::Hash(H256::default());
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec![],
+        preimages: vec![vec![]], // vec!(1u8, 2u8, 3u8, 4u8)
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("86b4b84950016d711732617d2501bd22e41614535f2705a65bd5b0e95c992a44");
+    assert_eq!(hash, expected);
+}
+
+// Adding a signature to SatisfiedPolicy of PolicyHash should have no effect
+#[test]
+fn test_satisfied_policy_encode_hash_frivulous_signature() {
+    let policy = SpendPolicy::Hash(H256::default());
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec!(Signature::from_bytes(
+            &hex::decode("105641BF4AE119CB15617FC9658BEE5D448E2CC27C9BC3369F4BA5D0E1C3D01EBCB21B669A7B7A17CF8457189EAA657C41D4A2E6F9E0F25D0996D3A17170F309").unwrap()).unwrap()),
+        preimages: vec!(vec!(1u8, 2u8, 3u8, 4u8)),
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("7424653d0ca3ffded9a029bebe75f9ae9c99b5f284e23e9d07c0b03456f724f9");
+    assert_eq!(hash, expected);
+}
+
+#[test]
+fn test_satisfied_policy_encode_hash() {
+    let policy = SpendPolicy::Hash(H256::default());
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec![],
+        preimages: vec![vec![1u8, 2u8, 3u8, 4u8]],
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("7424653d0ca3ffded9a029bebe75f9ae9c99b5f284e23e9d07c0b03456f724f9");
+    assert_eq!(hash, expected);
+}
+
+#[test]
+fn test_satisfied_policy_encode_unlock_condition_standard() {
+    let pubkey = PublicKey::from_bytes(
+        &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
+    )
+    .unwrap();
+
+    let unlock_condition = UnlockCondition::new(vec![pubkey], 0, 1);
+
+    let policy = SpendPolicy::UnlockConditions(unlock_condition);
+
+    let signature = Signature::from_bytes(
+        &hex::decode("105641BF4AE119CB15617FC9658BEE5D448E2CC27C9BC3369F4BA5D0E1C3D01EBCB21B669A7B7A17CF8457189EAA657C41D4A2E6F9E0F25D0996D3A17170F309").unwrap()).unwrap();
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec![signature],
+        preimages: vec![],
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("c749f9ac53395ec557aed7e21d202f76a58e0de79222e5756b27077e9295931f");
+    assert_eq!(hash, expected);
+}
+
+#[test]
+fn test_satisfied_policy_encode_unlock_condition_complex() {
+    let pubkey0 = PublicKey::from_bytes(
+        &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
+    )
+    .unwrap();
+    let pubkey1 = PublicKey::from_bytes(
+        &hex::decode("06C87838297B7BB16AB23946C99DFDF77FF834E35DB07D71E9B1D2B01A11E96D").unwrap(),
+    )
+    .unwrap();
+    let pubkey2 = PublicKey::from_bytes(
+        &hex::decode("BE043906FD42297BC0A03CAA6E773EF27FC644261C692D090181E704BE4A88C3").unwrap(),
+    )
+    .unwrap();
+
+    let unlock_condition = UnlockCondition::new(vec![pubkey0, pubkey1, pubkey2], 77777777, 3);
+
+    let policy = SpendPolicy::UnlockConditions(unlock_condition);
+
+    let sig0 = Signature::from_bytes(
+        &hex::decode("105641BF4AE119CB15617FC9658BEE5D448E2CC27C9BC3369F4BA5D0E1C3D01EBCB21B669A7B7A17CF8457189EAA657C41D4A2E6F9E0F25D0996D3A17170F309").unwrap()).unwrap();
+    let sig1 = Signature::from_bytes(
+        &hex::decode("0734761D562958F6A82819474171F05A40163901513E5858BFF9E4BD9CAFB04DEF0D6D345BACE7D14E50C5C523433B411C7D7E1618BE010A63C55C34A2DEE70A").unwrap()).unwrap();
+    let sig2 = Signature::from_bytes(
+        &hex::decode("482A2A905D7A6FC730387E06B45EA0CF259FCB219C9A057E539E705F60AC36D7079E26DAFB66ED4DBA9B9694B50BCA64F1D4CC4EBE937CE08A34BF642FAC1F0C").unwrap()).unwrap();
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec![sig0, sig1, sig2],
+        preimages: vec![],
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("13806b6c13a97478e476e0e5a0469c9d0ad8bf286bec0ada992e363e9fc60901");
+    assert_eq!(hash, expected);
+}
+
+#[test]
+fn test_satisfied_policy_encode_threshold_simple() {
+    let sub_policy = SpendPolicy::Hash(H256::default());
+    let policy = SpendPolicy::Threshold(PolicyTypeThreshold {
+        n: 1,
+        of: vec![sub_policy],
+    });
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec![],
+        preimages: vec![vec![1u8, 2u8, 3u8, 4u8]],
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("50f4808b0661f56842472aed259136a43ed2bd7d59a88a3be28de9883af4a92d");
+    assert_eq!(hash, expected);
+}
+
+#[test]
+fn test_satisfied_policy_encode_threshold_atomic_swap_success() {
+    let alice_pubkey = PublicKey::from_bytes(
+        &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
+    )
+    .unwrap();
+    let bob_pubkey = PublicKey::from_bytes(
+        &hex::decode("06C87838297B7BB16AB23946C99DFDF77FF834E35DB07D71E9B1D2B01A11E96D").unwrap(),
+    )
+    .unwrap();
+
+    let secret_hash = H256::from("0100000000000000000000000000000000000000000000000000000000000000");
+
+    let policy = spend_policy_atomic_swap_success(alice_pubkey, bob_pubkey, 77777777, secret_hash);
+    let signature = Signature::from_bytes(
+        &hex::decode("105641BF4AE119CB15617FC9658BEE5D448E2CC27C9BC3369F4BA5D0E1C3D01EBCB21B669A7B7A17CF8457189EAA657C41D4A2E6F9E0F25D0996D3A17170F309").unwrap()).unwrap();
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec![signature],
+        preimages: vec![vec![1u8, 2u8, 3u8, 4u8]],
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("c835e516bbf76602c897a9160c17bfe0e4a8bc9044f62b3e5e45a381232a2f86");
+    assert_eq!(hash, expected);
+}
+
+#[test]
+fn test_satisfied_policy_encode_threshold_atomic_swap_refund() {
+    let alice_pubkey = PublicKey::from_bytes(
+        &hex::decode("0102030000000000000000000000000000000000000000000000000000000000").unwrap(),
+    )
+    .unwrap();
+    let bob_pubkey = PublicKey::from_bytes(
+        &hex::decode("06C87838297B7BB16AB23946C99DFDF77FF834E35DB07D71E9B1D2B01A11E96D").unwrap(),
+    )
+    .unwrap();
+
+    let secret_hash = H256::from("0100000000000000000000000000000000000000000000000000000000000000");
+
+    let policy = spend_policy_atomic_swap_refund(alice_pubkey, bob_pubkey, 77777777, secret_hash);
+    let signature = Signature::from_bytes(
+        &hex::decode("105641BF4AE119CB15617FC9658BEE5D448E2CC27C9BC3369F4BA5D0E1C3D01EBCB21B669A7B7A17CF8457189EAA657C41D4A2E6F9E0F25D0996D3A17170F309").unwrap()).unwrap();
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy,
+        signatures: vec![signature],
+        preimages: vec![vec![1u8, 2u8, 3u8, 4u8]],
+    };
+
+    let hash = Encoder::encode_and_hash(&satisfied_policy);
+    let expected = H256::from("8975e8cf990d5a20d9ec3dae18ed3b3a0c92edf967a8d93fcdef6a1eb73bb348");
+    assert_eq!(hash, expected);
+}
+
+#[test]
 fn test_siacoin_input_encode_v2() {
-    let policy = SpendPolicy::Above(0);
+    let sub_policy = SpendPolicy::Hash(H256::default());
+    let policy = SpendPolicy::Threshold(PolicyTypeThreshold {
+        n: 1,
+        of: vec![sub_policy],
+    });
+
+    let satisfied_policy = SatisfiedPolicy {
+        policy: policy.clone(),
+        signatures: vec![],
+        preimages: vec![vec![1u8, 2u8, 3u8, 4u8]],
+    };
 
     let vin = SiacoinInputV2 {
         parent: SiacoinElement {
             state_element: StateElement {
                 id: H256::default(),
                 leaf_index: 0,
-                merkle_proof: vec![],
+                merkle_proof: vec![H256::default()],
             },
             siacoin_output: SiacoinOutput {
                 value: 1.into(),
-                address: Address::from_str(
-                    "addr:72b0762b382d4c251af5ae25b6777d908726d75962e5224f98d7f619bb39515dd64b9a56043a",
-                )
-                .unwrap(),
+                address: policy.address(),
             },
             maturity_height: 0,
         },
-        satisfied_policy: SatisfiedPolicy {
-            policy,
-            signatures: vec![],
-            preimages: vec![],
-        },
+        satisfied_policy,
     };
     let vin_wrapped = SiacoinInput::V2(vin);
 
     let hash = Encoder::encode_and_hash(&vin_wrapped);
-    let expected = H256::from("2f806f905436dc7c5079ad8062467266e225d8110a3c58d17628d609cb1c99d0");
+    let expected = H256::from("a8ab11b91ee19ce68f2d608bd4d19212841842f0c50151ae4ccb8e9db68cd6c4");
     assert_eq!(hash, expected);
 }
