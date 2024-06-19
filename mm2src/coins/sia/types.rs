de74bb857df4a74d0094fc1fcf6bbd9261a12e0c
Alright <mzlot555@gmail.com>
2024-06-19 14:57:53 -0400

    BlockID and Event serde

--- mm2src/coins/sia/types.rs
+++ mm2src/coins/sia/types.rs
@@ -1,14 +1,83 @@
-use crate::sia::encoding::{Encodable, Encoder};
+use crate::sia::encoding::{Encodable, Encoder, SiaHash};
 use crate::sia::address::Address;
 use crate::sia::transaction::{FileContractElementV1, FileContractElementV2, SiacoinElement, SiafundElement, TransactionV1, TransactionV2};
 use rpc::v1::types::H256;
 use chrono::{DateTime, Utc};
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize, Deserializer, Serializer};
+use serde_json::Value;
+use serde_with::{FromInto, serde_as};
+use std::convert::From;
+use std::fmt;
+use std::str::FromStr;
 
 
-pub type BlockID = H256;
+#[derive(Clone, Debug)]
+pub struct BlockID(pub H256);
 
-#[derive(Clone, Deserialize, Serialize)]
+impl From<BlockID> for H256 {
+    fn from(sia_hash: BlockID) -> Self {
+        sia_hash.0
+    }
+}
+
+impl From<H256> for BlockID {
+    fn from(h256: H256) -> Self {
+        BlockID(h256)
+    }
+}
+
+impl<'de> Deserialize<'de> for BlockID {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct BlockIDVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for BlockIDVisitor {
+            type Value = BlockID;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a string prefixed with 'bid:' and followed by a 64-character hex string")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                if let Some(hex_str) = value.strip_prefix("bid:") {
+                    H256::from_str(hex_str)
+                        .map(BlockID)
+                        .map_err(|_| E::invalid_value(
+                            serde::de::Unexpected::Str(value),
+                            &self,
+                        ))
+                } else {
+                    Err(E::invalid_value(
+                        serde::de::Unexpected::Str(value),
+                        &self,
+                    ))
+                }
+            }
+        }
+
+        deserializer.deserialize_str(BlockIDVisitor)
+    }
+}
+
+impl Serialize for BlockID {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&format!("{}", self))
+    }
+}
+
+impl fmt::Display for BlockID {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { write!(f, "bid:{}", self.0) }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct ChainIndex {
     pub height: u64,
     pub id: BlockID,
@@ -18,23 +87,17 @@ pub struct ChainIndex {
 impl Encodable for ChainIndex {
     fn encode(&self, encoder: &mut Encoder) {
         encoder.write_u64(self.height);
-        self.id.encode(encoder);
+        let block_id: H256 = self.id.clone().into();
+        block_id.encode(encoder);
     }
-}
-
-#[derive(Clone, Deserialize, Serialize)]
-pub enum EventDataEnum {
-    Payout(EventPayout),
-    V2Transaction(EventV2Transaction),
-    V2FileContractResolution(EventV2ContractResolution),
-    V1Transaction(EventV1Transaction),
-    V1FileContractResolution(EventV1ContractResolution)
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct EventV1Transaction {
     pub transaction: TransactionV1,
+    #[serde(rename = "spentSiacoinElements")]
     pub spent_siacoin_elements: Vec<SiacoinElement>,
+    #[serde(rename = "spentSiafundElements")]
     pub spent_siafund_elements: Vec<SiafundElement>,
 }
 
@@ -61,18 +124,82 @@ pub struct EventV2ContractResolution {
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct EventPayout {
+    #[serde(rename = "siacoinElement")]
     pub siacoin_element: SiacoinElement,
 }
 
 #[serde_as]
 #[derive(Clone, Debug, Serialize)]
 pub struct Event {
+    #[serde_as(as = "FromInto<SiaHash>")]
     pub id: H256,
     pub index: ChainIndex,
     pub timestamp: DateTime<Utc>,
+    #[serde(rename = "maturityHeight")]
     pub maturity_height: u64,
     #[serde(rename = "type")]
     pub event_type: String,
-    pub data: EventDataEnum,
+    pub data: EventDataWrapper,
     pub relevant: Option<Vec<Address>>,
 }
+
+impl<'de> Deserialize<'de> for Event {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize, Debug)]
+        struct EventHelper {
+            id: SiaHash,
+            index: ChainIndex,
+            timestamp: DateTime<Utc>,
+            #[serde(rename = "maturityHeight")]
+            maturity_height: u64,
+            #[serde(rename = "type")]
+            event_type: String,
+            data: Value,
+            relevant: Option<Vec<Address>>,
+        }
+
+        let helper = EventHelper::deserialize(deserializer)?;
+        println!("helper.data {:?}", helper.data);
+        let event_data = match helper.event_type.as_str() {
+            "miner" => serde_json::from_value::<EventPayout>(helper.data)
+                .map(EventDataWrapper::MinerPayout)
+                .map_err(serde::de::Error::custom),
+            "foundation" => serde_json::from_value::<EventPayout>(helper.data)
+                .map(EventDataWrapper::FoundationPayout)
+                .map_err(serde::de::Error::custom),
+            "siafundClaim" => serde_json::from_value::<EventPayout>(helper.data)
+                .map(EventDataWrapper::ClaimPayout)
+                .map_err(serde::de::Error::custom),
+            "v1Transaction" => serde_json::from_value::<EventV1Transaction>(helper.data)
+                .map(EventDataWrapper::V1Transaction)
+                .map_err(serde::de::Error::custom),
+            // Add other type mappings here...
+            _ => Err(serde::de::Error::unknown_variant(&helper.event_type, &["Payout", "V2Transaction", "V2FileContractResolution", "V1Transaction", "V1FileContractResolution"])),
+        }?;
+
+        Ok(Event {
+            id: helper.id.into(),
+            index: helper.index,
+            timestamp: helper.timestamp,
+            maturity_height: helper.maturity_height,
+            event_type: helper.event_type,
+            data: event_data,
+            relevant: helper.relevant,
+        })
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum EventDataWrapper {
+    MinerPayout(EventPayout),
+    FoundationPayout(EventPayout),
+    ClaimPayout(EventPayout),
+    V2Transaction(EventV2Transaction),
+    V2FileContractResolution(EventV2ContractResolution),
+    V1Transaction(EventV1Transaction),
+    V1FileContractResolution(EventV1ContractResolution)
+}
+
