fd794cad536de44f2d7e4e3a0fb71a6306238e86
mj-blockydevs <165827926+mj-blockydevs@users.noreply.github.com>
2024-06-25 13:16:29 +0200

    Update solana-entrypoint.sh

--- scripts/ci/solana-entrypoint.sh
+++ scripts/ci/solana-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-solana-test-validator > validator.log 2>&1 &
+
+# Starting Solana Local Node
+solana-test-validator > /var/log/validator.log 2>&1 &
 sleep 10
-solana program deploy /usr/src/satomic-swap/target/sbf-solana-solana/release/satomic_swap.so > deploy.log
-PROGRAM_ID=$(grep -oP '(?<=Program Id: )\w+' deploy.log)
-solana airdrop 10 > airdrop.log
-export PROGRAM_ID=$PROGRAM_ID
+
+# Deploying the program
+solana config set --url http://127.0.0.1:8899 && \
+solana-keygen new --no-passphrase --outfile /root/.config/solana/id.json && \
+solana airdrop 10 --url http://127.0.0.1:8899 && \
+solana program deploy --keypair /root/.config/solana/id.json /tmp/program.so > /var/log/deploy.log
+export PROGRAM_ID=$(grep -oP '(?<=Program Id: )\w+' /var/log/deploy.log)
 
 sleep infinity
-# cd /usr/src/komodo-defi-framework/mm2src/
-# cargo test
+# Running tests
+cd /usr/src/komodo-defi-framework/mm2src/coins && cargo test --features enable-solana
