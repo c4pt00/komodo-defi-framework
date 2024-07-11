6e9d63b51aed57c1f1a31409455938f5e31014bb
Alright <mzlot555@gmail.com>
2024-07-11 15:00:37 -0400

    fix Contract Resolution event deserialization

--- mm2src/coins/sia/types.rs
+++ mm2src/coins/sia/types.rs
@@ -1,7 +1,8 @@
 use crate::sia::address::Address;
-use crate::sia::encoding::{Encodable, Encoder, SiaHash};
-use crate::sia::transaction::{FileContractElementV1, FileContractElementV2, SiacoinElement, SiafundElement,
-                              TransactionV1, TransactionV2};
+use crate::sia::{Signature};
+use crate::sia::encoding::{Encodable, Encoder, SiaHash, SiaSignature};
+use crate::sia::transaction::{FileContractElementV1, V2FileContractElement, SiacoinElement, SiafundElement,
+                              TransactionV1, TransactionV2, Currency, V2FileContract, StateElement};
 use chrono::{DateTime, Utc};
 use rpc::v1::types::H256;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
@@ -98,13 +99,6 @@ pub struct EventV1ContractResolution {
     pub missed: bool,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct EventV2ContractResolution {
-    pub file_contract: FileContractElementV2,
-    pub resolution: String, // TODO stub; should be enum
-    pub siacoin_element: SiacoinElement,
-    pub missed: bool,
-}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct EventPayout {
@@ -146,7 +140,6 @@ impl<'de> Deserialize<'de> for Event {
         }
 
         let helper = EventHelper::deserialize(deserializer)?;
-        println!("helper.data {:?}", helper.data);
         let event_data = match helper.event_type.as_str() {
             "miner" => serde_json::from_value::<EventPayout>(helper.data)
                 .map(EventDataWrapper::MinerPayout)
@@ -162,6 +155,12 @@ impl<'de> Deserialize<'de> for Event {
                 .map_err(serde::de::Error::custom),
             "v2Transaction" => serde_json::from_value::<TransactionV2>(helper.data)
                 .map(EventDataWrapper::V2Transaction)
+                .map_err(serde::de::Error::custom),
+            // "v1ContractResolution" => serde_json::from_value::<EventV1ContractResolution>(helper.data)
+            //     .map(EventDataWrapper::V1FileContractResolution)
+            //     .map_err(serde::de::Error::custom),
+            "v2ContractResolution" => serde_json::from_value::<V2FileContractResolution>(helper.data)
+                .map(EventDataWrapper::V2FileContractResolution)
                 .map_err(serde::de::Error::custom),
             // Add other type mappings here...
             _ => Err(serde::de::Error::unknown_variant(&helper.event_type, &[
@@ -191,7 +190,98 @@ pub enum EventDataWrapper {
     FoundationPayout(EventPayout),
     ClaimPayout(EventPayout),
     V2Transaction(TransactionV2),
-    V2FileContractResolution(EventV2ContractResolution),
+    V2FileContractResolution(V2FileContractResolution),
     V1Transaction(EventV1Transaction),
-    V1FileContractResolution(EventV1ContractResolution),
+    EventV1ContractResolution(EventV1ContractResolution),
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct V2FileContractResolution {
+    pub parent: V2FileContractElement,
+    pub resolution: V2FileContractResolutionWrapper,
+}
+
+impl<'de> Deserialize<'de> for V2FileContractResolution {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize, Debug)]
+        struct V2FileContractResolutionHelper {
+            parent: V2FileContractElement,
+            #[serde(rename = "type")]
+            resolution_type: String,
+            resolution: Value,
+        }
+
+        let helper = V2FileContractResolutionHelper::deserialize(deserializer)?;
+        println!("type: {} helper.data: {:?}", helper.resolution_type.as_str(), helper.resolution);
+        let resolution_data = match helper.resolution_type.as_str() {
+            "renewal" => serde_json::from_value::<V2FileContractRenewal>(helper.resolution)
+                .map(V2FileContractResolutionWrapper::Renewal)
+                .map_err(serde::de::Error::custom),
+            "storage proof" => serde_json::from_value::<V2StorageProof>(helper.resolution)
+                .map(V2FileContractResolutionWrapper::StorageProof)
+                .map_err(serde::de::Error::custom),
+            // expiration is a special case because it has no data. It is just an empty map.
+            "expiration" => match &helper.resolution {
+                Value::Object(map) if map.is_empty() => Ok(V2FileContractResolutionWrapper::Expiration(V2FileContractExpiration)),
+                _ => Err(serde::de::Error::custom("expected an empty map for expiration")),
+            },
+            // "finalization"
+            _ => Err(serde::de::Error::unknown_variant(&helper.resolution_type, &[
+                "renewal",
+            ])),
+        }?;
+
+        Ok(V2FileContractResolution {
+            parent: helper.parent,
+            resolution: resolution_data,
+        })
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub enum V2FileContractResolutionWrapper {
+    Finalization(V2FileContractFinalization),
+    Renewal(V2FileContractRenewal),
+    StorageProof(V2StorageProof),
+    Expiration(V2FileContractExpiration)
+}
+
+type V2FileContractFinalization = V2FileContract;
+
+#[derive(Clone, Debug, Serialize)]
+pub struct V2FileContractExpiration;
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct V2FileContractRenewal {
+    final_revision: V2FileContract,
+    new_contract: V2FileContract,
+    renter_rollover: Currency,
+    host_rollover: Currency,
+    #[serde_as(as = "FromInto<SiaSignature>")]
+    renter_signature: Signature,
+    #[serde_as(as = "FromInto<SiaSignature>")]
+    host_signature: Signature,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ChainIndexElement {
+    #[serde(flatten)]
+    state_element: StateElement,
+    chain_index: ChainIndex,
+}
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct V2StorageProof {
+    proof_index: ChainIndexElement,
+    #[serde_as(as = "[_; 64]")] // FIXME placeholder serde impl
+    leaf: [u8; 64],
+    proof: Vec<H256>,
 }
