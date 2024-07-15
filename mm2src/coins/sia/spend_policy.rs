840a841db8229183d4427f3e58ff7b98cae52c86
Alright <mzlot555@gmail.com>
2024-07-15 19:29:43 -0400

    remove SpendPolicy miniscript-like parser; refactor UnlockKey

--- mm2src/coins/sia/spend_policy.rs
+++ mm2src/coins/sia/spend_policy.rs
@@ -4,98 +4,15 @@ use crate::sia::blake2b_internal::{public_key_leaf, sigs_required_leaf, standard
 use crate::sia::encoding::{Encodable, Encoder, PrefixedH256, PrefixedPublicKey};
 use crate::sia::specifier::Specifier;
 use ed25519_dalek::PublicKey;
-use nom::branch::alt;
-use nom::bytes::complete::{tag, take_while_m_n};
-use nom::character::complete::{char, digit1, multispace0};
+use nom::bytes::complete::{take_while_m_n, take_until, take_while};
+use nom::character::complete::{char};
 use nom::combinator::all_consuming;
 use nom::combinator::map_res;
-use nom::multi::separated_list0;
-use nom::sequence::{delimited, preceded, separated_pair};
 use nom::IResult;
 use rpc::v1::types::H256;
-use serde::{Deserialize, Serialize};
-use serde_with::serde_as;
+use serde::{Deserialize, Serialize, Deserializer, Serializer};
 use std::str::FromStr;
-
-// parse 32 bytes of hex to &str
-fn parse_hex_str(input: &str) -> IResult<&str, &str> {
-    all_consuming(take_while_m_n(64, 64, |c: char| c.is_digit(16)))(input)
-}
-
-// parse 32 bytes of hex to Vec<u8>
-fn parse_hex(input: &str) -> IResult<&str, Vec<u8>> {
-    all_consuming(map_res(take_while_m_n(64, 64, |c: char| c.is_digit(16)), hex::decode))(input)
-}
-
-fn parse_u64(input: &str) -> IResult<&str, u64> { map_res(digit1, |s: &str| s.parse::<u64>())(input) }
-
-fn parse_above(input: &str) -> IResult<&str, SpendPolicy> {
-    let parse_whitespace = delimited(multispace0, parse_u64, multispace0);
-    let (input, value) = delimited(tag("above("), parse_whitespace, char(')'))(input)?;
-    Ok((input, SpendPolicy::Above(value)))
-}
-
-fn parse_after(input: &str) -> IResult<&str, SpendPolicy> {
-    let parse_whitespace = delimited(multispace0, parse_u64, multispace0);
-    let (input, value) = delimited(tag("after("), parse_whitespace, char(')'))(input)?;
-    Ok((input, SpendPolicy::After(value)))
-}
-
-fn parse_opaque(input: &str) -> IResult<&str, SpendPolicy> {
-    let parse_hash = map_res(parse_hex_str, H256::from_str);
-    let parse_prefix = preceded(tag("0x"), parse_hash);
-    let parse_whitespace = delimited(multispace0, parse_prefix, multispace0);
-    let (input, h256) = delimited(tag("opaque("), parse_whitespace, tag(")"))(input)?;
-    Ok((input, SpendPolicy::Opaque(h256)))
-}
-
-fn parse_hash(input: &str) -> IResult<&str, SpendPolicy> {
-    let parse_hash = map_res(parse_hex_str, H256::from_str);
-    let parse_prefix = preceded(tag("0x"), parse_hash);
-    let parse_whitespace = delimited(multispace0, parse_prefix, multispace0);
-    let (input, h256) = delimited(tag("h("), parse_whitespace, tag(")"))(input)?;
-    Ok((input, SpendPolicy::Hash(h256)))
-}
-
-fn parse_public_key(input: &str) -> IResult<&str, SpendPolicy> {
-    let parse_public_key = map_res(parse_hex, |bytes: Vec<u8>| PublicKey::from_bytes(&bytes));
-    let parse_prefix = preceded(tag("0x"), parse_public_key);
-    let (input, public_key) = delimited(tag("pk("), parse_prefix, char(')'))(input)?;
-    Ok((input, SpendPolicy::PublicKey(public_key)))
-}
-
-fn parse_threshold(input: &str) -> IResult<&str, SpendPolicy> {
-    let parse_threshold = separated_pair(
-        map_res(digit1, |s: &str| s.parse::<u8>()),
-        char(','),
-        delimited(tag("["), separated_list0(char(','), parse_spend_policy), tag("]")),
-    );
-    let (input, (n, of)) = delimited(tag("thresh("), parse_threshold, tag(")"))(input)?;
-    Ok((input, SpendPolicy::Threshold { n, of }))
-}
-
-fn parse_spend_policy(input: &str) -> IResult<&str, SpendPolicy> {
-    let parse_policy = alt((
-        parse_above,
-        parse_after,
-        parse_public_key,
-        parse_hash,
-        parse_threshold,
-        parse_opaque,
-        // parse_unlock_condition, // TODO this may still be in flux from Sia devs
-    ));
-    // drop whitespace characters before and after the policy
-    delimited(multispace0, parse_policy, multispace0)(input)
-}
-
-impl SpendPolicy {
-    pub fn from_str(input: &str) -> Result<SpendPolicy, nom::Err<nom::error::Error<&str>>> {
-        match all_consuming(parse_spend_policy)(input) {
-            Ok((_, policy)) => Ok(policy),
-            Err(e) => Err(e),
-        }
-    }
-}
+use std::fmt;
 
 const POLICY_VERSION: u8 = 1u8;
 
@@ -106,11 +23,11 @@ pub enum SpendPolicy {
     PublicKey(PublicKey),
     Hash(H256),
     Threshold { n: u8, of: Vec<SpendPolicy> },
-    Opaque(H256),
+    Opaque(Address),
     UnlockConditions(UnlockCondition), // For v1 compatibility
 }
 
-// serde_with is used to serialize/deserialize SpendPolicy with prefixed PublicKey and H256
+// Helper to serialize/deserialize SpendPolicy with prefixed PublicKey and H256
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 #[serde(tag = "type", content = "policy", rename_all = "camelCase")]
 pub enum SpendPolicyHelper {
@@ -118,8 +35,8 @@ pub enum SpendPolicyHelper {
     After(u64),
     Pk(PrefixedPublicKey),
     H(PrefixedH256),
-    Thresh { n: u8, of: Vec<SpendPolicy> },
-    Opaque(PrefixedH256),
+    Thresh { n: u8, of: Vec<SpendPolicyHelper> },
+    Opaque(Address),
     Uc(UnlockCondition), // For v1 compatibility
 }
 
@@ -130,16 +47,15 @@ impl From<SpendPolicyHelper> for SpendPolicy {
             SpendPolicyHelper::After(time) => SpendPolicy::After(time),
             SpendPolicyHelper::Pk(pk) => SpendPolicy::PublicKey(pk.0),
             SpendPolicyHelper::H(hash) => SpendPolicy::Hash(hash.0),
-            SpendPolicyHelper::Thresh { n, of } => SpendPolicy::Threshold { n, of },
-            SpendPolicyHelper::Opaque(hash) => SpendPolicy::Opaque(hash.0),
+            SpendPolicyHelper::Thresh { n, of } => {
+                SpendPolicy::Threshold { n, of: of.into_iter().map(SpendPolicy::from).collect() }
+            },
+            SpendPolicyHelper::Opaque(address) => SpendPolicy::Opaque(address),
             SpendPolicyHelper::Uc(uc) => SpendPolicy::UnlockConditions(uc),
         }
     }
 }
 
-// Go serializes SpendPolicy with custom logic
-// eg, "policy": "pk(0x8a88e3dd7409f195fd52db2d3cba5d72ca6709bf1d94121bf3748801b40f6f5c)"
-// see `func (p SpendPolicy) String()` in policy.go
 impl Encodable for SpendPolicy {
     fn encode(&self, encoder: &mut Encoder) {
         encoder.write_u8(POLICY_VERSION);
@@ -189,7 +105,7 @@ impl SpendPolicy {
             },
             SpendPolicy::Opaque(address) => {
                 encoder.write_u8(opcode);
-                encoder.write_slice(&address.0);
+                encoder.write_slice(&address.0.0);
             },
             SpendPolicy::UnlockConditions(unlock_condition) => {
                 encoder.write_u8(opcode);
@@ -198,7 +114,7 @@ impl SpendPolicy {
                 for uc in &unlock_condition.unlock_keys {
                     uc.encode(encoder);
                 }
-                encoder.write_u64(unlock_condition.sigs_required);
+                encoder.write_u64(unlock_condition.signatures_required);
             },
         }
     }
@@ -233,12 +149,12 @@ impl SpendPolicy {
 
     pub fn threshold(n: u8, of: Vec<SpendPolicy>) -> Self { SpendPolicy::Threshold { n, of } }
 
-    pub fn opaque(p: &SpendPolicy) -> Self { SpendPolicy::Opaque(p.address().0) }
+    pub fn opaque(p: &SpendPolicy) -> Self { SpendPolicy::Opaque(p.address()) }
 
     pub fn anyone_can_spend() -> Self { SpendPolicy::threshold(0, vec![]) }
 }
 
-pub fn opacify_policy(p: &SpendPolicy) -> SpendPolicy { SpendPolicy::Opaque(p.address().0) }
+pub fn opacify_policy(p: &SpendPolicy) -> SpendPolicy { SpendPolicy::Opaque(p.address()) }
 
 pub fn spend_policy_atomic_swap(alice: PublicKey, bob: PublicKey, lock_time: u64, hash: H256) -> SpendPolicy {
     let policy_after = SpendPolicy::After(lock_time);
@@ -282,10 +198,100 @@ pub fn spend_policy_atomic_swap_refund(alice: PublicKey, bob: PublicKey, lock_ti
 
 // Sia Go v1 technically supports arbitrary length public keys
 // We only support ed25519 but must be able to deserialize others
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+// This data structure deviates from the Go implementation
+#[derive(Clone, Debug, PartialEq)]
 pub enum UnlockKey {
     Ed25519(PublicKey),
-    Unsupported { algorithm: Specifier, public_key: Vec<u8> },
+    Unsupported{ algorithm: Specifier, public_key: Vec<u8> },
+}
+
+impl<'de> Deserialize<'de> for UnlockKey {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct UnlockKeyVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for UnlockKeyVisitor {
+            type Value = UnlockKey;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("a string representing a Sia v1 UnlockKey; most often 'ed25519:<hex>'")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                match UnlockKey::from_str(value) {
+                    Ok(key) => Ok(key),
+                    Err(e) => Err(E::custom(format!("failed to parse UnlockKey: {}", e.0))),
+                }
+            }
+        }
+
+        deserializer.deserialize_str(UnlockKeyVisitor)
+    }
+}
+
+impl Serialize for UnlockKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+fn parse_specifier(input: &str) -> IResult<&str, Specifier> {
+    let (input, prefix_str) = take_until(":")(input)?;
+    let specifier = Specifier::from_str(prefix_str);
+    let (input, _) = char(':')(input)?;
+    Ok((input, specifier))
+}
+
+fn parse_unlock_key(input: &str) -> IResult<&str, UnlockKey> {
+    let (input, specifier) = parse_specifier(input)?;
+    match specifier {
+        Specifier::Ed25519 => {
+            let (input, public_key) = map_res(all_consuming(map_res(take_while_m_n(64, 64, |c: char| c.is_digit(16)), hex::decode)), |bytes: Vec<u8>| PublicKey::from_bytes(&bytes))(input)?;
+            Ok((input, UnlockKey::Ed25519(public_key)))
+        },
+        _ => {
+            let (input, public_key) = all_consuming(map_res(take_while(|c: char| c.is_digit(16)), |hex_str: &str| {
+                hex::decode(hex_str)
+            }))(input)?;
+            Ok((input, UnlockKey::Unsupported {
+                algorithm: specifier,
+                public_key,
+            }))
+        },
+    }
+}
+
+#[derive(Debug)]
+pub struct UnlockKeyParseError(pub String);
+
+impl FromStr for UnlockKey {
+    type Err = UnlockKeyParseError;
+
+    fn from_str(input: &str) -> Result<UnlockKey, Self::Err>  {
+        match all_consuming(parse_unlock_key)(input) {
+            Ok((_, key)) => Ok(key),
+            Err(e) => Err(UnlockKeyParseError(e.to_string())), // TODO unit test to check how verbose or useful this is
+        }
+    }
+}
+
+impl fmt::Display for UnlockKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            UnlockKey::Ed25519(public_key) => write!(f, "ed25519:{}", hex::encode(public_key.as_bytes())),
+            UnlockKey::Unsupported { algorithm, public_key } => {
+                write!(f, "{}:{}", algorithm.to_str(), hex::encode(public_key))
+            }
+        }
+    }
 }
 
 impl Encodable for PublicKey {
@@ -308,12 +314,13 @@ impl Encodable for UnlockKey {
         }
     }
 }
-
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct UnlockCondition {
+    #[serde(rename = "publicKeys")]
     pub unlock_keys: Vec<UnlockKey>,
     pub timelock: u64,
-    pub sigs_required: u64,
+    pub signatures_required: u64,
 }
 
 impl Encodable for UnlockCondition {
@@ -323,12 +330,12 @@ impl Encodable for UnlockCondition {
         for unlock_key in &self.unlock_keys {
             unlock_key.encode(encoder);
         }
-        encoder.write_u64(self.sigs_required);
+        encoder.write_u64(self.signatures_required);
     }
 }
 
 impl UnlockCondition {
-    pub fn new(pubkeys: Vec<PublicKey>, timelock: u64, sigs_required: u64) -> Self {
+    pub fn new(pubkeys: Vec<PublicKey>, timelock: u64, signatures_required: u64) -> Self {
         // TODO check go implementation to see if there should be limitations or checks imposed here
         // eg, max number of keys, max sigs_required, etc
         let unlock_keys = pubkeys
@@ -339,7 +346,7 @@ impl UnlockCondition {
         UnlockCondition {
             unlock_keys,
             timelock,
-            sigs_required,
+            signatures_required,
         }
     }
 
@@ -347,14 +354,14 @@ impl UnlockCondition {
         UnlockCondition {
             unlock_keys: vec![UnlockKey::Ed25519(public_key)],
             timelock: 0,
-            sigs_required: 1,
+            signatures_required: 1,
         }
     }
 
     pub fn unlock_hash(&self) -> H256 {
         // almost all UnlockConditions are standard, so optimize for that case
         if let UnlockKey::Ed25519(public_key) = &self.unlock_keys[0] {
-            if self.timelock == 0 && self.unlock_keys.len() == 1 && self.sigs_required == 1 {
+            if self.timelock == 0 && self.unlock_keys.len() == 1 && self.signatures_required == 1 {
                 return standard_unlock_hash(&public_key);
             }
         }
@@ -367,7 +374,7 @@ impl UnlockCondition {
             accumulator.add_leaf(public_key_leaf(&unlock_key));
         }
 
-        accumulator.add_leaf(sigs_required_leaf(self.sigs_required));
+        accumulator.add_leaf(sigs_required_leaf(self.signatures_required));
         accumulator.root()
     }
 
