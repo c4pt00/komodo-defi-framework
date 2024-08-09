96d4855872c72408cd4a50a4bac29138d3ef0237
Mariusz Jasuwienas <mariusz.jasuwienas+instead@blockydevs.com>
2024-08-09 08:34:50 +0200

    feat(solana-tests): mint SPL to one account only
    Signed-off-by: Mariusz Jasuwienas <mariusz.jasuwienas+instead@blockydevs.com>

--- .docker/solana-accounts/mint.sh
+++ .docker/solana-accounts/mint.sh
@@ -4,13 +4,14 @@ MINT_AMOUNT_USDC=1000000000
 MINT_AMOUNT_ADEX=1000000000
 
 OWNER_KEYPAIR=/root/accounts/iguana
+ACCOUNT_FILE=/root/accounts/user3.json
 
 solana config set --url http://127.0.0.1:8899
 solana config set --keypair "$OWNER_KEYPAIR"
 
 USDC_TOKEN=$(spl-token create-token --decimals 6 --mint-authority "$OWNER_KEYPAIR" --fee-payer "$OWNER_KEYPAIR" | grep -o -E 'Creating token [A-Za-z0-9]+' | awk '{print $3}')
 ADEX_TOKEN=$(spl-token create-token --decimals 9 --mint-authority "$OWNER_KEYPAIR" --fee-payer "$OWNER_KEYPAIR" | grep -o -E 'Creating token [A-Za-z0-9]+' | awk '{print $3}')
-ACCOUNT_FILE=/root/accounts/user3.json
+
 spl-token create-account "$USDC_TOKEN" --owner "$OWNER_KEYPAIR"
 spl-token create-account "$ADEX_TOKEN" --owner "$OWNER_KEYPAIR"
 
@@ -21,16 +22,13 @@ if [ -z "$USDC_ASSOCIATED_TOKEN_ADDRESS" ]; then
   USDC_ASSOCIATED_TOKEN_ADDRESS=$(spl-token create-account "$USDC_TOKEN" --owner "$PUBKEY" --fee-payer "$OWNER_KEYPAIR" | grep -o -E 'Creating account [A-Za-z0-9]+' | awk '{print $3}')
 fi
 
-
 ADEX_ASSOCIATED_TOKEN_ADDRESS=$(spl-token accounts --owner "$PUBKEY" | grep "$ADEX_TOKEN" | awk '{print $1}')
 if [ -z "$ADEX_ASSOCIATED_TOKEN_ADDRESS" ]; then
   ADEX_ASSOCIATED_TOKEN_ADDRESS=$(spl-token create-account "$ADEX_TOKEN" --owner "$PUBKEY" --fee-payer "$OWNER_KEYPAIR" | grep -o -E 'Creating account [A-Za-z0-9]+' | awk '{print $3}')
 fi
+
 spl-token mint "$USDC_TOKEN" "$MINT_AMOUNT_USDC" --mint-authority "$OWNER_KEYPAIR" --recipient-owner "$PUBKEY" --fee-payer "$OWNER_KEYPAIR"
 spl-token mint "$ADEX_TOKEN" "$MINT_AMOUNT_ADEX" --mint-authority "$OWNER_KEYPAIR" --recipient-owner "$PUBKEY" --fee-payer "$OWNER_KEYPAIR"
-
-spl-token mint "$USDC_TOKEN" 100000
-spl-token mint "$ADEX_TOKEN" 100000
 
 echo "New token addresses:"
 echo "USDC Token Address: $USDC_TOKEN"
