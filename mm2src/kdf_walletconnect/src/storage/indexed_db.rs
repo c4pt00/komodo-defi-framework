8fc7620595ef55840a6826c79be82848b15aaa1c
Samuel Onoja <samiodev@icloud.com>
2024-10-11 10:43:51 +0100

    fix wasm clippy

--- mm2src/kdf_walletconnect/src/storage/indexed_db.rs
+++ mm2src/kdf_walletconnect/src/storage/indexed_db.rs
@@ -55,11 +55,10 @@ impl IDBSessionStorage {
     }
 
     async fn lock_db(&self) -> MmResult<IDBSessionStorageLocked<'_>, WcIndexedDbError> {
-        Ok(self
-            .0
+        self.0
             .get_or_initialize()
             .await
-            .mm_err(|err| WcIndexedDbError::InternalError(err.to_string()))?)
+            .mm_err(|err| WcIndexedDbError::InternalError(err.to_string()))
     }
 }
 
@@ -76,7 +75,7 @@ impl WalletConnectStorageOps for IDBSessionStorage {
         let transaction = lock_db.get_inner().transaction().await?;
         let session_table = transaction.table::<Session>().await?;
         session_table
-            .replace_item_by_unique_index("topic", session.topic.clone(), &session)
+            .replace_item_by_unique_index("topic", session.topic.clone(), session)
             .await?;
 
         Ok(())
@@ -115,35 +114,5 @@ impl WalletConnectStorageOps for IDBSessionStorage {
         Ok(())
     }
 
-    async fn update_session(&self, session: &Session) -> MmResult<(), Self::Error> { self.save_session(&session).await }
-}
-
-pub(crate) mod crypto {
-    use js_sys::{JsString, Object, Reflect, Uint8Array};
-
-    fn rsa_oaep_params() -> Object {
-        // Create a new empty JavaScript object
-        let rsa_oaep_params = Object::new();
-
-        // "name": "RSA-OAEP"
-        let _ = Reflect::set(&rsa_oaep_params, &JsString::from("name"), &JsString::from("RSA-OAEP"));
-
-        // "modulusLength": 2048
-        let _ = Reflect::set(&rsa_oaep_params, &JsString::from("modulusLength"), &2048.into());
-
-        // "publicExponent": new Uint8Array([0x01, 0x00, 0x01])
-        let public_exponent = Uint8Array::new_with_length(3);
-        public_exponent.copy_from(&[0x01, 0x00, 0x01]);
-
-        let _ = Reflect::set(&rsa_oaep_params, &JsString::from("publicExponent"), &public_exponent);
-
-        // Create the "hash" sub-object
-        let hash_obj = Object::new();
-        let _ = Reflect::set(&hash_obj, &JsString::from("name"), &JsString::from("SHA-256"));
-
-        // "hash": {"name": "SHA-256"}
-        let _ = Reflect::set(&rsa_oaep_params, &JsString::from("hash"), &hash_obj);
-
-        rsa_oaep_params
-    }
+    async fn update_session(&self, session: &Session) -> MmResult<(), Self::Error> { self.save_session(session).await }
 }
