ad0741d22e65d7fba26724a116db81a36d4f0984
shamardy <shamardy@yahoo.com>
2024-12-10 19:03:39 +0200

    remove core import in mm2.rs

--- mm2src/mm2_main/src/mm2.rs
+++ mm2src/mm2_main/src/mm2.rs
@@ -38,7 +38,6 @@
 #[macro_use] extern crate serde_json;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate ser_error_derive;
-extern crate core;
 #[cfg(test)] extern crate mm2_test_helpers;
 
 #[cfg(not(target_arch = "wasm32"))] use common::block_on;
